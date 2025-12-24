acedd3c53257a2eb31781792b105a985b48f9e51
Andrew Read <andrew81244@outlook.com>
2025-12-25 01:59:34 +0800

    testcase for angular separation, needs external verification

--- ForthAstroCalc/ForthAstroCalc_test3.f
+++ ForthAstroCalc/ForthAstroCalc_test3.f
@@ -1,27 +1,20 @@
 \ automated test for ForthAstroCalc.f
 
 need FiniteFractions
-need simple-tester
+need simple-tester 
+include E:\coding\AstroCalc\ForthAstroCalc\ForthAstroCalc.f
 
-\ Point A RA, Dec                      Point B (RA, Dec)                    Expected Separation
-
- 05 00 00 RA +20 00 00 Dec 05 00 00 RA +20 00 00 Dec ang_sep ~. cr  
- 10 12 00 RA +15 00 00 Dec 10 12 30 RA +15 00 00 Dec ang_sep ~. cr  
- 12 00 00 RA -05 00 00 Dec 12 00 00 RA -04 59 59 Dec ang_sep ~. cr  
- 03 00 00 RA +10 00 00 Dec 03 30 00 RA +12 00 00 Dec ang_sep ~. cr  
- 18 00 00 RA +30 00 00 Dec 20 00 00 RA -10 00 00 Dec ang_sep ~. cr  
- 06 00 00 RA +00 00 00 Dec 07 00 00 RA +00 00 00 Dec ang_sep ~. cr  
- 02 00 00 RA +85 00 00 Dec 14 00 00 RA +85 00 00 Dec ang_sep ~. cr 
- 00 00 00 RA +00 00 00 Dec 12 00 00 RA +00 00 00 Dec ang_sep ~. cr 
- 04 00 00 RA +25 00 00 Dec 04 00 00 RA -25 00 00 Dec ang_sep ~. cr 
- 18 36 56 RA +38 47 01 Dec 19 50 47 RA +08 52 06 Dec ang_sep ~. cr  
-
- 0 7 15
- 1 59 59                                      
- 7 37 42
- 49 19 26
- 15 0 0
- 10 0 0
- 180 0 0
- 50 0 0
- 34 11 45
+CR
+Tstart
+T{ 05 00 00 RA +20 00 00 Dec 05 00 00 RA +20 00 00 Dec ang_sep }T 00 00 00  ~ ==     
+T{ 10 12 00 RA +15 00 00 Dec 10 12 30 RA +15 00 00 Dec ang_sep }T 00 07 15  ~ ==     
+T{ 12 00 00 RA -05 00 00 Dec 12 00 00 RA -04 59 59 Dec ang_sep }T 01 59 59  ~ ==    
+T{ 03 00 00 RA +10 00 00 Dec 03 30 00 RA +12 00 00 Dec ang_sep }T 07 37 42  ~ ==    
+T{ 18 00 00 RA +30 00 00 Dec 20 00 00 RA -10 00 00 Dec ang_sep }T 49 19 26  ~ ==     
+T{ 06 00 00 RA +00 00 00 Dec 07 00 00 RA +00 00 00 Dec ang_sep }T 15 00 00  ~ ==    
+T{ 02 00 00 RA +85 00 00 Dec 14 00 00 RA +85 00 00 Dec ang_sep }T 10 00 00  ~ ==    
+T{ 00 00 00 RA +00 00 00 Dec 12 00 00 RA +00 00 00 Dec ang_sep }T 180 00 00 ~ ==   
+T{ 04 00 00 RA +25 00 00 Dec 04 00 00 RA -25 00 00 Dec ang_sep }T 50 00 00  ~ ==  
+T{ 18 36 56 RA +38 47 01 Dec 19 50 47 RA +08 52 06 Dec ang_sep }T 34 11 45  ~ ==
+CR
+Tend
